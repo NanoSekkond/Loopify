b09a7fdbc6875dc01af665b849493c9e0404dd54
Nano <mudkip2435@gmail.com>
2024-03-13 12:30:10 -0300

    Deleted wrong file

--- owo.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-from time import sleep
-
-while(True):
-    now = int(datetime.now().timestamp() * 1000)
-    arle = int(datetime(2024, 3, 11, 6, 0, 0, 0).timestamp() * 1000)
-    print(arle - now)
